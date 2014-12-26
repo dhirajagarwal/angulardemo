d89a48da7a9971ecf0f50af2f7cbc95b89e4bd38
dhirajagarwal <dhirajagarwal@live.com>
2014-12-26 10:12:02 +0530

    Added angular files

---
+++ app.js
@@ -0,0 +1,4 @@
+/**
+ * Created by dhirajagarwal on 25/12/14.
+ */
+var app = angular.module('myapp',['Controllers'])
---
+++ controllers.js
@@ -0,0 +1,8 @@
+/**
+ * Created by dhirajagarwal on 25/12/14.
+ */
+var controllers = angular.module('Controllers',[])
+
+controllers.controller('MainCtrl',function($scope){
+    $scope.dummy = 345;
+})
